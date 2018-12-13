dd56cd5df42ca196a47ee9ef3fa5e387f3499f5b
Lukas Burger <lukas.burger@yale.edu>
2018-12-12 19:57:11 -0500

    added sentiment analysis

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask, request, jsonify
-from nltk.corpus import twitter_samples
+from nltk.corpus import twitter_samples, stopwords
+from nltk.tokenize import TweetTokenizer
+from nltk.stem import PorterStemmer
+from nltk import classify
+from nltk import NaiveBayesClassifier
 import logging
 import tweepy 
 import os
 import sys
+import re
+import string
+from random import shuffle 
 
 app = Flask(__name__)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -16,6 +23,82 @@ consumer_secret = os.environ['consumer_secret']
 access_key = os.environ['access_key']
 access_secret = os.environ['access_secret']
 
+tweet_tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+stemmer = PorterStemmer()
+
+stopwords_english = stopwords.words('english')
+pos_tweets = twitter_samples.strings('positive_tweets.json')
+neg_tweets = twitter_samples.strings('negative_tweets.json')
+
+ 
+# Happy Emoticons
+emoticons_happy = set([
+    ':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
+    ':^)', ':-D', ':D', '8-D', '8D', 'x-D', 'xD', 'X-D', 'XD', '=-D', '=D',
+    '=-3', '=3', ':-))', ":'-)", ":')", ':*', ':^*', '>:P', ':-P', ':P', 'X-P',
+    'x-p', 'xp', 'XP', ':-p', ':p', '=p', ':-b', ':b', '>:)', '>;)', '>:-)',
+    '<3'
+    ])
+ 
+# Sad Emoticons
+emoticons_sad = set([
+    ':L', ':-/', '>:/', ':S', '>:[', ':@', ':-(', ':[', ':-||', '=L', ':<',
+    ':-[', ':-<', '=\\', '=/', '>:(', ':(', '>.<', ":'-(", ":'(", ':\\', ':-c',
+    ':c', ':{', '>:\\', ';('
+    ])
+ 
+# all emoticons (happy + sad)
+emoticons = emoticons_happy.union(emoticons_sad)
+ 
+def clean_tweets(tweet):
+    # remove stock market tickers like $GE
+    tweet = re.sub(r'\$\w*', '', tweet)
+ 
+    # remove old style retweet text "RT"
+    tweet = re.sub(r'^RT[\s]+', '', tweet)
+ 
+    # remove hyperlinks
+    tweet = re.sub(r'https?:\/\/.*[\r\n]*', '', tweet)
+    
+    # remove hashtags
+    # only removing the hash # sign from the word
+    tweet = re.sub(r'#', '', tweet)
+ 
+    # tokenize tweets
+    tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
+    tweet_tokens = tokenizer.tokenize(tweet)
+ 
+    tweets_clean = []    
+    for word in tweet_tokens:
+        if (word not in stopwords_english and # remove stopwords
+              word not in emoticons and # remove emoticons
+                word not in string.punctuation): # remove punctuation
+            stem_word = stemmer.stem(word) # stemming word
+            tweets_clean.append(stem_word)
+ 
+    return tweets_clean
+
+def bag_of_words(tweet):
+    words = clean_tweets(tweet)
+    words_dictionary = dict([word, True] for word in words)    
+    return words_dictionary
+
+pos_tweets_set = []
+for tweet in pos_tweets:
+    pos_tweets_set.append((bag_of_words(tweet), 'pos'))    
+ 
+# negative tweets feature set
+neg_tweets_set = []
+for tweet in neg_tweets:
+    neg_tweets_set.append((bag_of_words(tweet), 'neg'))
+
+shuffle(pos_tweets_set)
+shuffle(neg_tweets_set)
+ 
+train_set = pos_tweets_set + neg_tweets_set
+
+classifier = NaiveBayesClassifier.train(train_set)
+ 
 
 def get_all_tweets(screen_name):
 	#Twitter only allows access to a users most recent 3240 tweets with this method
@@ -57,8 +140,21 @@ def get_all_tweets(screen_name):
 def add_user():
     username = request.args['username']
     tweets = get_all_tweets(username)
+    sentiment_tweets = []
 
-    return jsonify(tweets)
+    for tweet in tweets:
+    	tweet_words = bag_of_words(tweet[2])
+    	prob_result = classifier.prob_classify(tweet_words)
+
+    	new_data = {}
+    	new_data['id'] = tweet[0]
+    	new_data['time'] = tweet[1]
+    	new_data['text'] = tweet[2]
+    	new_data['sentiment'] = prob_result.max()
+
+    	sentiment_tweets.append(new_data)
+
+    return jsonify(sentiment_tweets)
 
 if __name__ == '__main__':
     app.run(debug=True)
