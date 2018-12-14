d04e768386e5af5f8ec8fdc41e28c96610fd0388
Lukas Burger <lukas.burger@yale.edu>
2018-12-14 18:29:24 -0500

    spaces

--- app.py
+++ app.py
@@ -109,40 +109,41 @@ def bag_of_words(tweet):
  
 
 def get_all_tweets(screen_name, num_tweets):
-	#Twitter only allows access to a users most recent 3240 tweets with this method
-	
-	#authorize twitter, initialize tweepy
-	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-	auth.set_access_token(access_key, access_secret)
-	api = tweepy.API(auth)
-	
-	#initialize a list to hold all the tweepy Tweets
-	alltweets = []	
-	
-	#make initial request for most recent tweets (200 is the maximum allowed count)
-	new_tweets = api.user_timeline(screen_name = screen_name,count=200)
-	
-	#save most recent tweets
-	alltweets.extend(new_tweets)
-	
-	#save the id of the oldest tweet less one
-	oldest = alltweets[-1].id - 1
-	count = 0
-	#keep grabbing tweets until there are no tweets left to grab
-	while len(new_tweets) > 0 and count <= num_tweets:
-		#all subsiquent requests use the max_id param to prevent duplicates
-		new_tweets = api.user_timeline(screen_name = screen_name,count=100,max_id=oldest)
-		#save most recent tweets
-		alltweets.extend(new_tweets)
-		#update the id of the oldest tweet less one
-		oldest = alltweets[-1].id - 1
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+    
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+    
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []  
+    
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+    
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+    
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+    count = 0
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0 and count <= num_tweets:
         count += 100
-		
-	
-	#transform the tweepy tweets into a 2D array that will populate the csv	
-	outtweets = [[tweet.id_str, tweet.created_at.strftime('%m/%d/%Y'), tweet.text] for tweet in alltweets]
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=100,max_id=oldest)
+        #save most recent tweets
+        alltweets.extend(new_tweets)
 
-	return outtweets
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+        
+    
+    #transform the tweepy tweets into a 2D array that will populate the csv 
+    outtweets = [[tweet.id_str, tweet.created_at.strftime('%m/%d/%Y'), tweet.text] for tweet in alltweets]
+
+    return outtweets
 
 
 @app.route("/tweet", methods=["GET"])
