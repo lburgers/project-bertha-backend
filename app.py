c29a9f241eca2cb730fa724e712521832226d687
Lukas Burger <lukas.burger@yale.edu>
2018-12-19 13:07:35 -0500

    comments

--- app.py
+++ app.py
@@ -18,8 +18,10 @@ from random import shuffle
 
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
+# initialize vader sentiment analyzer
 analyzer = SentimentIntensityAnalyzer()
 
+# initialize flask service
 app = Flask(__name__)
 CORS(app)
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
@@ -32,6 +34,7 @@ consumer_secret = os.environ['consumer_secret']
 access_key = os.environ['access_key']
 access_secret = os.environ['access_secret']
 
+# load stemmer, tokenizer and corpi
 tweet_tokenizer = TweetTokenizer(preserve_case=False, strip_handles=True, reduce_len=True)
 stemmer = PorterStemmer()
 
@@ -44,6 +47,7 @@ sports_words = defaultdict(str)
 technology_words = defaultdict(str)
 art_words = defaultdict(str)
 
+# load political words into lists
 for line in open('./topics/political.txt'):
     political_words[line.strip().lower()] = True
 
@@ -75,7 +79,8 @@ emoticons_sad = set([
  
 # all emoticons (happy + sad)
 emoticons = emoticons_happy.union(emoticons_sad)
- 
+
+# remove uneeded information from tweeets
 def clean_tweets(tweet):
     # remove stock market tickers like $GE
     tweet = re.sub(r'\$\w*', '', tweet)
@@ -104,36 +109,11 @@ def clean_tweets(tweet):
  
     return tweets_clean
 
+# convert words into dict
 def bag_of_words(tweet):
     words = clean_tweets(tweet)
     words_dictionary = dict([word, True] for word in words)
     return words_dictionary
-
-# def classify_topic(tweet):
-#     words = clean_tweets(tweet)
-
-#     num_political = 0
-#     num_sports = 0
-#     num_tech = 0
-      
-#     for word in words:
-#         if (political_words[word]):
-#             num_political += 1
-#         if (sports_words[word]):
-#             num_sports += 1
-#         if (technology_words[word]):
-#             num_tech += 1
-
-#     max_val = max(num_political, num_sports, num_tech)
-#     if max_val == 0:
-#         return "none"
-
-#     if max_val == num_political:
-#         return "political"
-#     elif max_val == num_sports:
-#         return "sports"
-#     elif max_val == num_tech:
-#         return "num_tech"
 
 
 def get_all_tweets(screen_name, num_tweets):
@@ -173,14 +153,37 @@ def get_all_tweets(screen_name, num_tweets):
 
     return outtweets
 
+# positivie tweets feature set
+pos_tweets_set = []
+for tweet in pos_tweets:
+    pos_tweets_set.append((bag_of_words(tweet), 'pos'))    
+ 
+# negative tweets feature set
+neg_tweets_set = []
+for tweet in neg_tweets:
+    neg_tweets_set.append((bag_of_words(tweet), 'neg'))
+
+# shuffle them
+shuffle(pos_tweets_set)
+shuffle(neg_tweets_set)
+ 
+train_set = pos_tweets_set + neg_tweets_set
+# train NB classifier
+# classifier = NaiveBayesClassifier.train(train_set)
+
+# gets test accuracy of 0.74
 
 @app.route("/tweet", methods=["GET"])
 def add_user():
+
+    # parse twitter username and number of tweets from GET request
     username = request.args['username']
     num_tweets = request.args['num_tweets']
+
+    # load all tweets
     tweets = get_all_tweets(username, num_tweets)
 
-
+    # initialize variables
     data = {}
 
     time_series = []
@@ -199,14 +202,17 @@ def add_user():
     pos_tweets = []
     neg_tweets = []
 
-    # sentiment_tweets = []
     for tweet in tweets:
+
+        # naive bayes classifier
         # tweet_words = bag_of_words(tweet[2])
+        # prob_result = classifier.prob_classify(tweet_words)
+
         words = clean_tweets(tweet[2])
       
+        # count topic words
         for word in words:
             word = word.lower()
-
             if (political_words[word]):
                 num_political += 1
 
@@ -219,6 +225,7 @@ def add_user():
             if (art_words[word]):
                 num_art += 1
 
+        # get vader sentiment data on tweeet
         prob_result = analyzer.polarity_scores(tweet[2])
 
         new_data = {}
@@ -226,6 +233,8 @@ def add_user():
         new_data['time'] = tweet[1]
         new_data['text'] = tweet[2]
 
+        # keep track of amount of positive and negative tweets
+        # and keep list of all pos/neg tweets
         if prob_result['compound'] >= 0.1:
             pos_tweets.append((tweet, prob_result['compound']))
             num_pos += 1
@@ -235,11 +244,13 @@ def add_user():
         else:
             num_neu += 1
 
+        # track most pos/neg tweet
         if prob_result['compound'] > max_pos:
             max_pos = prob_result['compound']
         elif prob_result['compound'] < max_neg:
             max_neg = prob_result['compound']
 
+        # graph data
         entry = {}
         entry['time'] = tweet[1]
         entry['score'] = prob_result['compound']
@@ -248,7 +259,6 @@ def add_user():
         entry['neu'] = prob_result['neu']
         time_series.append(entry)
 
-    # data['tweets'] = sentiment_tweets
     data['time_series'] = time_series
     
     data['num_pos'] = num_pos
@@ -260,6 +270,7 @@ def add_user():
     data['num_political'] = num_political
     data['num_sports'] = num_sports
 
+    # sort and grab top 5 pos/neg tweets
     pos_tweets = sorted(pos_tweets, key=lambda k: k[1], reverse=True)[:6]
     pos_tweets, _ = zip(*pos_tweets)
     neg_tweets = sorted(neg_tweets, key=lambda k: k[1])[:6]
@@ -268,6 +279,7 @@ def add_user():
     data['most_positive'] = pos_tweets
     data['most_negative'] = neg_tweets
 
+    # return as JSON blob to react JS app
     return jsonify(data)
 
 if __name__ == '__main__':
